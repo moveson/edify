cb4e1df4c9906afc10659948884f8cd80bdffc06
moveson <mark.oveson@gmail.com>
2022-03-11 21:44:56 -0700

    Follow-up to initial commit

---
+++ config/routes/madmin.rb
@@ -0,0 +1,16 @@
+# Below are the routes for madmin
+namespace :madmin do
+  resources :announcements
+  resources :services
+  resources :users
+  namespace :active_storage do
+    resources :blobs
+  end
+  namespace :active_storage do
+    resources :variant_records
+  end
+  namespace :active_storage do
+    resources :attachments
+  end
+  root to: "dashboard#show"
+end
