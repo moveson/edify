b7f023ecfb018081dd434cec867e8a37b9370ed8
moveson <mark.oveson@gmail.com>
2022-03-17 01:31:19 -0600

    Seed admin user with confirmed account

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(first_name: "Admin", last_name: "User", email: "admin@example.com", password: "password", admin: true)
+User.create(
+  first_name: "Admin",
+  last_name: "User",
+  email: "admin@example.com",
+  password: "password",
+  admin: true,
+  confirmed_at: Time.current
+)
