5a1afbb4fe32cd804cacbd6b45d9189502016e92
moveson <mark.oveson@gmail.com>
2022-03-21 22:27:09 -0600

    Working sort and filter for member list

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module FormatHelper
+  def filtered_and_total_count(filtered_count, total_count, model_name)
+    total_string = pluralize_with_delimiter(total_count, model_name)
+    filtered_count == total_count ? total_string : "#{filtered_count} of #{total_string}"
+  end
+
+  def pluralize_with_delimiter(count, singular, plural = nil)
+    pluralize(number_with_delimiter(count), singular, plural)
+  end
+
   def pretty_duration(seconds)
     return "--:--" unless seconds.present?
 
