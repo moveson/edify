ec4007c0235ff88600f8d6b8b01be3c0ec74b5cc
moveson <mark.oveson@gmail.com>
2022-03-27 09:58:18 -0600

    Remove unnecessary tooltips

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -4,75 +4,51 @@ module LinkHelper
   def link_to_member_delete(member, options = {})
     html_class = options[:class]
     url = member_path(member)
-    tooltip = "Delete this member"
     options = { method: :delete,
                 data: { confirm: "This will delete the member and cannot be undone. Are you sure you want to proceed?",
-                        turbo: false,
-                        toggle: :tooltip,
-                        placement: :bottom,
-                        "original-title" => tooltip },
-                class: ["btn btn-danger btn-sm has-tooltip", html_class].compact.join(" ") }
+                        turbo: false },
+                class: ["btn btn-danger btn-sm", html_class].compact.join(" ") }
     link_to fa_icon("trash"), url, options
   end
 
   def link_to_member_edit(member, options = {})
     html_class = options[:class]
     url = edit_member_path(member)
-    tooltip = "Edit this member"
-    options = { data: { toggle: :tooltip,
-                        placement: :bottom,
-                        "original-title" => tooltip },
-                class: ["btn btn-primary btn-sm has-tooltip", html_class].compact.join(" ") }
+    options = { class: ["btn btn-primary btn-sm", html_class].compact.join(" ") }
     link_to fa_icon("pencil-alt"), url, options
   end
 
   def link_to_note_delete(note, options = {})
     html_class = options[:class]
     url = member_note_path(note.member, note)
-    tooltip = "Delete this note"
     options = { method: :delete,
                 data: { confirm: "This will delete the note and cannot be undone. Are you sure you want to proceed?",
-                        turbo: false,
-                        toggle: :tooltip,
-                        placement: :bottom,
-                        "original-title" => tooltip },
-                class: ["btn btn-danger btn-sm has-tooltip", html_class].compact.join(" ") }
+                        turbo: false },
+                class: ["btn btn-danger btn-sm", html_class].compact.join(" ") }
     link_to fa_icon("trash"), url, options
   end
 
   def link_to_note_edit(note, options = {})
     html_class = options[:class]
     url = edit_member_note_path(note.member, note)
-    tooltip = "Edit this note"
-    options = { data: { toggle: :tooltip,
-                        placement: :bottom,
-                        "original-title" => tooltip },
-                class: ["btn btn-primary btn-sm has-tooltip", html_class].compact.join(" ") }
+    options = { class: ["btn btn-primary btn-sm", html_class].compact.join(" ") }
     link_to fa_icon("pencil-alt"), url, options
   end
 
   def link_to_talk_delete(talk, options = {})
     html_class = options[:class]
     url = talk_path(talk)
-    tooltip = "Delete this talk"
     options = { method: :delete,
                 data: { confirm: "This will delete the talk and cannot be undone. Are you sure you want to proceed?",
-                        turbo: false,
-                        toggle: :tooltip,
-                        placement: :bottom,
-                        "original-title" => tooltip },
-                class: ["btn btn-danger btn-sm has-tooltip", html_class].compact.join(" ") }
+                        turbo: false },
+                class: ["btn btn-danger btn-sm", html_class].compact.join(" ") }
     link_to fa_icon("trash"), url, options
   end
 
   def link_to_talk_edit(talk, options = {})
     html_class = options[:class]
     url = edit_talk_path(talk)
-    tooltip = "Edit this talk"
-    options = { data: { toggle: :tooltip,
-                        placement: :bottom,
-                        "original-title" => tooltip },
-                class: ["btn btn-primary btn-sm has-tooltip", html_class].compact.join(" ") }
+    options = { class: ["btn btn-primary btn-sm", html_class].compact.join(" ") }
     link_to fa_icon("pencil-alt"), url, options
   end
 end
