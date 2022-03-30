7052679027b3a98dd7321b3c7a6d8467b2561700
moveson <mark.oveson@gmail.com>
2022-03-30 07:55:08 -0600

    Better formatting for meeting cards and remove the calendar link

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -4,11 +4,11 @@ module IconHelper
   def status_icon(status)
     case status
     when :ok
-      fa_icon("check-circle", type: :regular, size: :lg, class: "text-success")
+      fa_icon("check-circle", type: :regular, class: "text-success")
     when :empty
-      fa_icon("dot-circle", type: :regular, size: :lg, class: "text-danger")
+      fa_icon("dot-circle", type: :regular, class: "text-danger")
     when :questionable
-      fa_icon("dot-circle", type: :regular, size: :lg, class: "text-secondary")
+      fa_icon("dot-circle", type: :regular, class: "text-secondary")
     else
       raise ArgumentError, "Unknown status: #{status}"
     end
