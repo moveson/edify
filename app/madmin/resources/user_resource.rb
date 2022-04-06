4e167d1b226fe56cd952be06f1cabac5618f53e9
moveson <mark.oveson@gmail.com>
2022-04-06 03:09:55 -0600

    Limit and reorder attributes in user resource

--- app/madmin/resources/user_resource.rb
+++ app/madmin/resources/user_resource.rb
@@ -2,15 +2,13 @@ class UserResource < Madmin::Resource
   # Attributes
   attribute :id, form: false
   attribute :email
-  attribute :confirmed_at
-  attribute :encrypted_password
-  attribute :reset_password_token
-  attribute :reset_password_sent_at
-  attribute :remember_created_at
   attribute :first_name
   attribute :last_name
-  attribute :announcements_last_read_at
+  attribute :confirmed_at
   attribute :admin
+  attribute :reset_password_sent_at
+  attribute :remember_created_at
+  attribute :announcements_last_read_at
   attribute :created_at, form: false
   attribute :updated_at, form: false
   attribute :avatar, index: false
