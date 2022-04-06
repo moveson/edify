c8e34e1eaf4819e90e647fb5d3914e87db6f6cd8
moveson <mark.oveson@gmail.com>
2022-04-06 01:34:27 -0600

    Use environment variable directly to avoid loading order issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: ::EdifyConfig.base_uri, protocol: "http"}
+  config.action_mailer.default_url_options = {host: ENV["BASE_URI"], protocol: "http"}
 
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
--- config/initializers/01_edify_config.rb
+++ config/initializers/01_edify_config.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class EdifyConfig
-  def self.base_uri
-    ENV["BASE_URI"]
-  end
-
   def self.sendgrid_api_key
     ::ENV["SENDGRID_API_KEY"]
   end
