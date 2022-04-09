c73bfd95227adc6660f5edcf051e8e903ebc2216
moveson <mark.oveson@gmail.com>
2022-04-09 02:53:36 -0600

    Deliver new user notifications by Twilio

--- app/notifications/new_user_admin_notification.rb
+++ app/notifications/new_user_admin_notification.rb
@@ -3,6 +3,7 @@
 class NewUserAdminNotification < Noticed::Base
   deliver_by :database
   deliver_by :email, mailer: "AdminMailer", format: :format_for_email
+  deliver_by :twilio, format: :format_for_twilio
 
   param :user
 
@@ -10,6 +11,14 @@ class NewUserAdminNotification < Noticed::Base
     {
       user: params[:user],
       message: message,
+    }
+  end
+
+  def format_for_twilio
+    {
+      Body: "#{message} #{params[:user].name} (#{params[:user].email})",
+      From: Rails.application.credentials.twilio[:phone_number],
+      To: recipient.phone
     }
   end
 
