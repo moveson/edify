4af7bf62918713e253eec167265d487c8203802a
moveson <mark.oveson@gmail.com>
2022-04-09 03:34:14 -0600

    Fix default user selection

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -5,8 +5,8 @@ module MeetingsHelper
     users = current_unit.users.where.not(id: current_user.id).to_a
     users.unshift(current_user)
     select_array = users.map { |user| [user.name, user.id] }
-    select_array.push(["Unassigned", nil])
-    default_value = meeting.persisted? ? meeting.user_id : meeting.user_id || current_user.id
+    select_array.push(["Unassigned", ""])
+    default_value = meeting.persisted? ? meeting.user_id.to_s : (meeting.user_id || current_user.id).to_s
 
     options_for_select(select_array, default_value)
   end
