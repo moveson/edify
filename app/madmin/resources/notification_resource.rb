1252e2a9d918375e46c3bdae7c19859f064ee807
moveson <mark.oveson@gmail.com>
2022-04-09 01:06:37 -0600

    Make it possible to edit notifications within madmin

--- app/madmin/resources/notification_resource.rb
+++ app/madmin/resources/notification_resource.rb
@@ -2,13 +2,14 @@ class NotificationResource < Madmin::Resource
   # Attributes
   attribute :id, form: false
   attribute :type
-  attribute :params
+  attribute :params, form: false
   attribute :read_at
   attribute :created_at, form: false
   attribute :updated_at, form: false
 
   # Associations
-  attribute :recipient
+  attribute :recipient_type
+  attribute :recipient_id
 
   # Uncomment this to customize the display name of records in the admin area.
   # def self.display_name(record)
