6d196f3809b6e1d58fd334caf26133b390b78637
moveson <mark.oveson@gmail.com>
2022-04-10 09:48:28 -0600

    First basic spec for Meeting model

---
+++ spec/models/meeting_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ::Meeting, type: :model do
+  subject { described_class.new(date: date) }
+  describe "#not_yet_occurred?" do
+    let(:result) { subject.not_yet_occurred? }
+
+    context "when the meeting is in the past" do
+      let(:date) { 3.days.ago }
+      it { expect(result).to eq(false) }
+    end
+
+    context "when the meeting is on the current date" do
+      let(:date) { Date.current }
+      it { expect(result).to eq(true) }
+    end
+
+    context "when the meeting is in the future" do
+      let(:date) { 3.days.from_now }
+      it { expect(result).to eq(true) }
+    end
+  end
+end
