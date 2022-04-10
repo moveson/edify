71b5c6350de476697755c95e4b594895b2f4abfe
moveson <mark.oveson@gmail.com>
2022-04-10 07:42:10 -0600

    Change order of attribute display for notification resource

--- app/madmin/resources/notification_resource.rb
+++ app/madmin/resources/notification_resource.rb
@@ -2,14 +2,12 @@ class NotificationResource < Madmin::Resource
   # Attributes
   attribute :id, form: false
   attribute :type
+  attribute :recipient_type
+  attribute :recipient_id
   attribute :params, form: false
   attribute :read_at
   attribute :created_at, form: false
   attribute :updated_at, form: false
-
-  # Associations
-  attribute :recipient_type
-  attribute :recipient_id
 
   # Uncomment this to customize the display name of records in the admin area.
   # def self.display_name(record)
