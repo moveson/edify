6c3355407d6815232074494ef398d3be3110eef2
moveson <mark.oveson@gmail.com>
2022-04-25 02:52:32 -0600

    Generate madmin resource for import jobs

---
+++ app/madmin/resources/import_job_resource.rb
@@ -0,0 +1,34 @@
+class ImportJobResource < Madmin::Resource
+  # Attributes
+  attribute :id, form: false
+  attribute :status
+  attribute :status_text
+  attribute :row_count, form: false
+  attribute :succeeded_count, form: false
+  attribute :failed_count, form: false
+  attribute :ignored_count, form: false
+  attribute :started_at
+  attribute :elapsed_seconds
+  attribute :error_message
+  attribute :logs
+  attribute :created_at, form: false
+  attribute :updated_at, form: false
+  attribute :raw_data, index: false
+
+  # Associations
+  attribute :unit
+
+  # Uncomment this to customize the display name of records in the admin area.
+  # def self.display_name(record)
+  #   record.name
+  # end
+
+  # Uncomment this to customize the default sort column and direction.
+  # def self.default_sort_column
+  #   "created_at"
+  # end
+  #
+  # def self.default_sort_direction
+  #   "desc"
+  # end
+end
--- config/routes/madmin.rb
+++ config/routes/madmin.rb
@@ -1,5 +1,6 @@
 # Below are the routes for madmin
 namespace :madmin do
+  resources :import_jobs
   resources :notifications
   resources :units
   resources :meetings
