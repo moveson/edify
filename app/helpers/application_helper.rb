813b3c98bd4f46e2f0c0742c289422f25a5cb2b5
moveson <mark.oveson@gmail.com>
2022-05-04 10:22:55 -0700

    Basic datepicker logic
    Credit Zsolt Benke https://decoding.io/2022/01/how-i-built-a-date-picker/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,34 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  def svg_icon(path, name, options = {})
+    width = options[:width].present? ? options[:width] : '16'
+    height = options[:height].present? ? options[:height] : '16'
+    css_class = options[:class].present? ? options[:class] : 'black'
+
+    tag = "<svg width=\"#{width}px\" height=\"#{height}px\" viewBox=\"0 0 #{width} #{height}\" class=\"icon #{css_class}\">" +
+      "<use xlink:href=\"#{ asset_path path }##{name}\"></use>" +
+      "</svg>"
+
+    tag.html_safe
+  end
+
+  def form_actions(inline: false, plain: false, centered: false, &block)
+    css_class = %w(form_actions)
+    css_class << 'inline' if inline
+    css_class << 'plain' if plain
+    css_class << 'centered' if centered
+    css_class = css_class.join(' ')
+    actions = content_tag(:div, capture(&block), class: 'inner')
+    content_tag(:div, actions, class: css_class)
+  end
+
+  def record_form_for(record, options = {}, &block)
+    options.merge! builder: RecordFormBuilder
+    options[:html] = options[:html] || {}
+    options[:html].merge! autocomplete: 'off'
+    options[:html].merge! class: 'record_form'
+    form_for record, options, &block
+  end
 end
---
+++ app/helpers/record_form_builder.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class RecordFormBuilder < ActionView::Helpers::FormBuilder
+  def date_picker_field(attribute, options = {})
+    @template.render partial: "shared/date_picker", locals: {
+      form: self,
+      attribute: attribute,
+      time: false,
+      options: options
+    }
+  end
+
+  def date_time_picker_field(attribute, options = {})
+    @template.render partial: "shared/date_picker", locals: {
+      form: self,
+      attribute: attribute,
+      time: true,
+      options: options
+    }
+  end
+end
