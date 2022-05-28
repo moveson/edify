5990d7e2da21f16b635c70cc80bb50f8863ee77d
moveson <mark.oveson@gmail.com>
2022-05-27 18:04:52 -0600

    Add system specs for updating and deleting a user

---
+++ spec/system/delete_user_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Update a user role", type: :system, js: true do
+  let(:unit) { units(:sunny_hills) }
+  let(:bishopric_user) { users(:sunny_bishopric) }
+  let(:music_user) { users(:sunny_music) }
+
+  scenario "Bishopric user deletes a user" do
+    login_as bishopric_user, scope: :user
+
+    visit users_path
+    music_user_element = page.find("#user_#{music_user.id}")
+    expect(music_user_element).to have_text(music_user.name)
+    delete_button = music_user_element.find(:xpath, "//a[@href='/users/#{music_user.id}']")
+
+    accept_alert do
+      delete_button.click
+    end
+
+    expect(page).to have_current_path(users_path)
+    expect(page).not_to have_text(music_user.name)
+    expect { music_user.reload }.to raise_error ActiveRecord::RecordNotFound
+  end
+end
---
+++ spec/system/update_user_role_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Update a user role", type: :system, js: true do
+  let(:unit) { units(:sunny_hills) }
+  let(:bishopric_user) { users(:sunny_bishopric) }
+  let(:music_user) { users(:sunny_music) }
+
+  scenario "Bishopric user changes a user from music to clerk" do
+    login_as bishopric_user, scope: :user
+
+    visit users_path
+    music_user.reload
+    expect(music_user.role).to eq("music")
+
+    music_user_element = page.find("#user_#{music_user.id}")
+    expect(music_user_element).to have_text(music_user.name)
+    edit_button = music_user_element.find(:xpath, "//a[@href='/users/#{music_user.id}/edit']")
+    edit_button.click
+
+    music_user_element.find("#user_role").find(:xpath, "option[contains(text(), 'Clerk')]").select_option
+    music_user_element.find('[type="submit"]').click
+
+    expect(page).to have_current_path(users_path)
+    expect(music_user_element).to have_text(music_user.name)
+    music_user.reload
+    expect(music_user.role).to eq("clerk")
+  end
+end
