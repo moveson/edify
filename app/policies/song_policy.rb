427a4de6d5ff91683fa3ba66d1a62a2da006c67a
moveson <mark.oveson@gmail.com>
2022-06-25 11:49:07 -0600

    Give permissions to music people to create, update, and delete songs

--- app/policies/song_policy.rb
+++ app/policies/song_policy.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class SongPolicy < ApplicationPolicy
+  def new?
+    user.music_editor?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    new?
+  end
+
+  def update?
+    new?
+  end
+
+  def destroy?
+    new?
+  end
 end
