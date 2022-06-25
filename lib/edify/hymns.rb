16ab1aa9a9d887d030b8febe2492e2f859107631
moveson <mark.oveson@gmail.com>
2022-06-25 12:00:57 -0600

    Sort hymn database by number

--- lib/edify/hymns.rb
+++ lib/edify/hymns.rb
@@ -12,10 +12,11 @@ module Edify
       end
 
       def load_database!
-        @database =
-          ::CSV.readlines(::Rails.root.join("lib/edify/db/hymns.txt"), :col_sep => "\t").map do |title, number|
-            [number.to_i, title]
-          end
+        array = ::CSV.readlines(::Rails.root.join("lib/edify/db/hymns.txt"), :col_sep => "\t").map do |title, number|
+          [number.to_i, title]
+        end
+
+        @database = array.sort
       end
     end
   end
