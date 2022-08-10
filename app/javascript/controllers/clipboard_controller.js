40ffe67bb1d72f3229ef7f5616fd795a81a67749
moveson <mark.oveson@gmail.com>
2022-08-09 21:21:46 -0600

    Flash the Copy button when activated

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,9 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["source"]
+    static targets = ["button", "source"]
 
     copy() {
         navigator.clipboard.writeText(this.sourceTarget.innerText)
+        this.flashButton()
+    }
+
+    flashButton() {
+        const button = this.buttonTarget;
+        const icon = button.querySelector("i")
+
+        button.classList.add("btn-primary")
+        button.classList.remove("btn-outline-primary")
+        icon.classList.add("fa-clipboard-check")
+        icon.classList.remove("fa-clipboard")
+
+        setTimeout(() => {
+            button.classList.remove("btn-primary")
+            button.classList.add("btn-outline-primary")
+            icon.classList.remove("fa-clipboard-check")
+            icon.classList.add("fa-clipboard")
+        }, 600)
     }
 }
