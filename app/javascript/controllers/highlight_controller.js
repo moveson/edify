cdc7856e43904bda516c338a65c75cd0381eed49
moveson <mark.oveson@gmail.com>
2023-08-28 10:46:43 -0600

    Highlight the Today indicator on page load

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -2,12 +2,11 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-    static targets = ["element"]
     static values = { created: Number }
 
     connect() {
         const five_seconds_ago = Math.round(Date.now() / 1000) - 5
-        const subjectElement = this.elementTarget
+        const subjectElement = this.element
 
         if (this.createdValue > five_seconds_ago) {
             subjectElement.classList.add("bg-highlight")
@@ -15,7 +14,7 @@ export default class extends Controller {
             setTimeout(function () {
                 subjectElement.classList.remove("bg-highlight");
                 subjectElement.classList.add("bg-highlight-faded");
-            }, 2000);
+            }, 500);
 
         }
     }
