b06e4bdaf7e518c8f42c71a879c7a72986635d1a
moveson <mark.oveson@gmail.com>
2023-08-28 23:12:39 -0600

    Use Intersection Observer to determine when to scroll more

--- app/javascript/controllers/meetings_controller.js
+++ app/javascript/controllers/meetings_controller.js
@@ -6,13 +6,34 @@ export default class extends Controller {
   ]
 
   connect() {
-    if (this.hasTodayIndicatorTarget) {
-      this.todayIndicatorTarget.scrollIntoView({ behavior: "smooth", block: "end" })
+    this.scrollTodayIntoView();
+  }
 
-      // Wait for scroll to finish, then scroll up a bit
-      setTimeout(() => {
-        window.scrollBy(0, 100)
-      }, 200)
+  scrollTodayIntoView() {
+    if (this.hasTodayIndicatorTarget) {
+      let options = {
+        rootMargin: "0px",
+        threshold: [0.5],
+      };
+
+      let observer = new IntersectionObserver(this.scrollMore, options);
+      observer.observe(this.todayIndicatorTarget);
+
+      this.todayIndicatorTarget.scrollIntoView({behavior: "smooth", block: "end"})
     }
+  }
+
+  scrollMore(entries, observer) {
+    // Scroll a little bit more when the entry comes into view
+    entries.forEach(entry => {
+      if (entry.intersectionRatio === 0) {
+        entry.target.needsMore = true;
+      }
+
+      if (entry.target.needsMore && entry.intersectionRatio > 0) {
+        window.scrollBy(0, 75);
+        observer.unobserve(entry.target)
+      }
+    })
   }
 }
