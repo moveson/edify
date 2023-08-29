6731c67683664417ab83a25cbcf591222bbb7c08
moveson <mark.oveson@gmail.com>
2023-08-28 21:14:22 -0600

    Scroll up a bit more after the Today indicator comes into view

--- app/javascript/controllers/meetings_controller.js
+++ app/javascript/controllers/meetings_controller.js
@@ -8,6 +8,11 @@ export default class extends Controller {
   connect() {
     if (this.hasTodayIndicatorTarget) {
       this.todayIndicatorTarget.scrollIntoView({ behavior: "smooth", block: "end" })
+
+      // Wait for scroll to finish, then scroll up a bit
+      setTimeout(() => {
+        window.scrollBy(0, 100)
+      }, 200)
     }
   }
 }
