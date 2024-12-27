06bb4041438207ec7bdb2413ffe35fc179cc0dde
moveson <mark.oveson@gmail.com>
2024-12-26 17:11:26 -0700

    Update config files for Rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,13 @@ module Edify
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
     config.application_name = Rails.application.class.module_parent_name
-    config.autoload_paths += Dir[Rails.root.join("lib/core_ext/**/*.rb").to_s].each { |l| require l }
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -21,5 +25,7 @@ module Edify
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    Dir[Rails.root.join("lib/core_ext/**/*.rb").to_s].each { |l| require l }
   end
 end
